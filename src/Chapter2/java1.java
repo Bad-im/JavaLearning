2ed3be911a0aa3697ac4108ddc024f414fd312b9
vadim <vadima019@gmail.com>
2024-09-20 16:03:41 +0800

    1

--- src/Chapter2/java1.java
+++ src/Chapter2/java1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class java1 {
 
     public static void main(String[] args) {
-        String developerName = "Иванов";
+        String developerName = "Астапов";
         long assignmentReceivedTime = System.currentTimeMillis();
         long assignmentSubmittedTime = System.currentTimeMillis();
 
