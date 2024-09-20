90ed730c6efee13ee2ac0720dc039de3bfc7f7de
vadim <vadima019@gmail.com>
2024-09-20 16:02:42 +0800

    1

--- src/Chapter2/java1.java
+++ src/Chapter2/java1.java
@@ -1,2 +1,39 @@
-package Chapter2;public class java1 {
+package Chapter2;
+
+import java.util.Scanner;
+
+public class java1 {
+
+    public static void main(String[] args) {
+        String developerName = "Иванов";
+        long assignmentReceivedTime = System.currentTimeMillis();
+        long assignmentSubmittedTime = System.currentTimeMillis();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество чисел: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Введите число " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        int minLength = Integer.MAX_VALUE;
+        int maxLength = Integer.MIN_VALUE;
+        int minLengthNumber = 0;
+        int maxLengthNumber = 0;
+        for (int number : numbers) {
+            int length = String.valueOf(number).length();
+            if (length < minLength) {
+                minLength = length;
+                minLengthNumber = number;
+            }
+            if (length > maxLength) {
+                maxLength = length;
+                maxLengthNumber = number;
+            }
+        }
+        System.out.println("Самое короткое число: " + minLengthNumber + ", длина: " + minLength);
+        System.out.println("Самое длинное число: " + maxLengthNumber + ", длина: " + maxLength);
+    }
 }
